894d0747ed494932808c5b0aefbe0aa47ace9626
mathunjoroge <mathunjoroge@gmail.com>
2020-09-04 10:57:42 +0300

    deleted accounts folder in sass

--- accounts/sass/accounts/record_mis_expense.php
+++
@@ -1,9 +0,0 @@
-  <div>
-    <form action="save_mis_expense.php" method="POST">
-  	<center><label>record expense</label></center>
-    <span><input type="text" name="name"> </span> 	
-  	<input type="number" name="amount" placeholder="enter amount"></br>
-<p>&nbsp;</p>
-<button class="btn btn-success" style="width: 90%;">submit</button>
-  	</form>
-  </div>
